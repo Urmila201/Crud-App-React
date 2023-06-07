5ac946ad1936bb0851c8bcbf3ee4b70b5cc4944f
UrmilaLandge <urmilalandge2001@gmail.com>
2023-06-07 11:54:05 +0530

    First Commit Crud-React

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+function App() {
+  const [columns, setColumns] = useState([]);
+  const [records, setRecords] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:3030/users')
+      .then(res => {
+        setColumns(Object.keys(res.data[0]));
+        setRecords(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  function handleSubmit(id) {
+    const conf = window.confirm("Do you want to delete?");
+    if (conf) {
+      axios.delete(`http://localhost:3030/users/${id}`)
+        .then(res => {
+          alert('Record has been deleted.');
+          navigate('/');
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
+  return (
+    <div className='container mt-5'>
+      <div className='text-end'><Link to='/create' className='btn btn-primary'>Add +</Link></div>
+      <table className='table'>
+        <thead>
+          <tr>
+            {columns.map((c, i) => (
+              <th key={i}>{c}</th>
+            ))}
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((d, i) => (
+            <tr key={i}>
+              <td>{d.id}</td>
+              <td>{d.name}</td>
+              <td>{d.email}</td>
+              <td>
+                <Link to={`/update/${d.id}`} className='btn btn-sm btn-success'>Update</Link>
+                <button onClick={() => handleSubmit(d.id)} className='btn btn-sm ms-1 btn-danger'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
